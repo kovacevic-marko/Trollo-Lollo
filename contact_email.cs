f1056931bdad8ecc7b7b321d463ed3fb32df3cfb
Marko Kovacevic <markokovacevic97@gmail.com>
2018-02-25 01:47:53 +0100

    contact_email

---
+++ contact_email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marko_kovacevic
+{
+    class contact_email
+    {
+        string name = "Marko";
+        string email = "markokovacevic97@gmail.com";
+    }
+}
